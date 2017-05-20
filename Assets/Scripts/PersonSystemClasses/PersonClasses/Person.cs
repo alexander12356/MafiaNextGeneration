e85b3bd095938f0201af5b10dde259b9f546d4c9
alexander12356 <alexander12356@inbox.ru>
2017-05-20 10:50:34 +0900

    Copy files to server

---
+++ Assets/Scripts/PersonSystemClasses/PersonClasses/Person.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Beansjam.PersonSystemClasses.PersonClasses
+{
+    public class Person : MonoBehaviour
+    {
+        private enum State
+        {
+            Patrol
+        }
+
+        private State m_State = State.Patrol;
+        private Vector3 m_MovingTarget;
+        private float MOVING_RANGE = 0.01f;
+
+        public Vector2 randomBounds;
+
+        private void Start()
+        {
+            m_MovingTarget = transform.position;
+        }
+
+        private void Update()
+        {
+            switch(m_State)
+            {
+                case State.Patrol:
+                    PatrolUpdate();
+                    break;
+            }
+        }
+
+        private void PatrolUpdate()
+        {
+            if (Vector3.Distance(transform.position,  m_MovingTarget) < MOVING_RANGE)
+            {
+                m_MovingTarget = PersonManager.Instance.GetRandomFromRect();
+            }
+            transform.position = Vector3.MoveTowards(transform.position, m_MovingTarget, Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/PersonSystemClasses/PersonManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using Beansjam.PersonSystemClasses.PersonClasses;
+
+namespace Beansjam.PersonSystemClasses
+{
+    public class PersonManager : MonoBehaviour
+    {
+        private static PersonManager m_Instance;
+
+        [SerializeField]
+        private Person m_SpeciePrefab;
+
+        public float SPECIES_SPAWN_TIME;
+        public float SpawnRadius;
+        public int PersonCount = 0;
+
+        public float CreatePersonTime = 1;
+        public float DeadPersonTime = 1;
+        public int InitPersonCount = 1000;
+
+        private List<Person> m_PersonList = new List<Person>();
+
+        public static PersonManager Instance
+        {
+            get
+            {
+                return m_Instance;
+            }
+        }
+
+        public void Awake()
+        {
+            m_Instance = this;
+        }
+
+        public void Start()
+        {
+            InitPersons(InitPersonCount);
+        }
+
+        private void InitPersons(int p_Count)
+        {
+            for (int i = 0; i < p_Count; i++)
+            {
+                CreateSpecies();
+            }
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        private void CreateSpecies()
+        {
+            var l_Specie = Instantiate(m_SpeciePrefab);
+            l_Specie.transform.position = GetRandomFromRect();
+        }
+
+        public Vector2 GetRandomFromRect()
+        {
+            return Random.insideUnitCircle * SpawnRadius;
+        }
+    }
+}
